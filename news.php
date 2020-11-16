a1f786c07b8c75ec2645eb25660efbd481368146
red171 <red171@applejuicenet.cc>
2020-11-16 11:15:21 +0100

    update news

--- news.php
+++ news.php
@@ -1,15 +1,14 @@
 <?php
 header('Content-Type: text/html; charset=ISO-8859-15');
 
-$version = $_REQUEST['version'] ?: '';
 
-$message = file_get_contents('news/_header.html');
-
-# zeige Versions spezifische Informationen als erstes an
-if (preg_match('#0.[\d]+.[\d]+.[\d]+#', $version) && file_exists(sprintf('news/%s.html', $version))) {
-    $message .= file_get_contents(sprintf('news/%s.html', $version));
-}
-
-$message .= file_get_contents('news/_footer.html');
-
-echo $message;
+echo file_get_contents(
+    sprintf('https://applejuicenetz.github.io/news/%s.html', $_REQUEST['version'] ?: '404'),
+    false,
+    stream_context_create(
+        [
+            'http' => [
+                'ignore_errors' => true,
+            ],
+        ]
+    ));
