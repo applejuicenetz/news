d60c1894bd6429d88716830ed807cc1c047d22ff
red171 <red171@applejuicenet.cc>
2023-03-17 16:32:42 +0100

    Update news.php

--- news.php
+++ news.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=ISO-8859-15');
 
 
 echo file_get_contents(
-    sprintf('https://applejuicenetz.github.io/news/%s.html', $_REQUEST['version'] ?: '404'),
+    sprintf('https://applejuicenetz.github.io/news/%s.html', $_REQUEST['version'] ?? '404'),
     false,
     stream_context_create(
         [
